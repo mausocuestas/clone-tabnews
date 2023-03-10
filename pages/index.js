b9300c1ffd8f80368ca6bb86b931c10d5a378372
mauso cuestas <mfuzetto.visa@gmail.com>
2023-03-10 11:12:04 +0000

    <> </>

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home(){
-    return <h1>Teste novo pelo celular. Fiz um commit and sync hahaha</h1>
+    return 
+    <>
+    <h1>Teste novo pelo celular. Fiz um commit and sync hahaha</h1>
+    </>
 }
 
 export default Home
